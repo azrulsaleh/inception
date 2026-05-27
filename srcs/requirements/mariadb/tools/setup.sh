ab059063dfd1c051deea6fe3a12a01a9f0d50f48
Azrul Saleh <azrulsaleh@me.com>
2026-05-28 00:22:47 +0800

    replaced admin credentials, named volume configuration, mariadb execution, wordpress hardcode version

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -23,6 +23,7 @@ mariadb -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
+mariadb-admin shutdown
 
 echo "MariaDB ready!"
-wait
+exec mariadbd --user=mysql --bind-address=0.0.0.0
